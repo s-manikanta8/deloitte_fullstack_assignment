1afd21eee5805cb567865c279ca4ce3dc07b8ed9
s-manikanta8 <msonga@deloitte.com>
2024-09-03 18:27:04 +0530

    Add files via upload

---
+++ assinment02/assignment1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string job;
+            int id1;
+            int sal;
+
+            Console.WriteLine("enter your name : ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("enter your id : ");
+            id1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter your job: ");
+            job = Console.ReadLine();
+            Console.WriteLine("enter your salary:");
+            sal = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("------- Employ Details -------");
+            Console.WriteLine("Employ Name : " + name);
+            Console.WriteLine("Employ Id : " + id1);
+            Console.WriteLine("Employ Job : " + job);
+            Console.WriteLine("Employ salary : " + sal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assinment02/assignment2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string pname;
+            int price;
+            int q;
+
+            Console.WriteLine("enter product id : ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter product name : ");
+            pname = Console.ReadLine();
+
+            Console.WriteLine("enter unit price : ");
+            price = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter quantity :");
+            q = int.Parse(Console.ReadLine());
+            int d = 0;
+            int ttlp = 0;
+            int p = 0;
+            if (q > 10)
+            {
+                p = 10;
+            }
+            if (q > 30)
+            {
+                p = 20;
+            }
+            if (q > 50)
+            {
+                p = 30;
+            }
+            if (q > 70)
+            {
+                p = 40;
+            }
+            d = (price * q) / p;
+            ttlp = (price * q) - d;
+            Console.WriteLine("------- bill -------");
+            Console.WriteLine("product id : " + id);
+            Console.WriteLine("product name : " + pname);
+            Console.WriteLine("unit price : " + price);
+            Console.WriteLine("quantity : " + q);
+            Console.WriteLine("total price : " + q * price);
+            Console.WriteLine("discount amount : " + d);
+            Console.WriteLine("final amount : " + ttlp);
+            Console.ReadLine();
+        }
+    }
+}
