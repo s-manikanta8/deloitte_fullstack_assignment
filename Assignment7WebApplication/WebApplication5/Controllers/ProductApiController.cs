92a7d3597cad1f3a1fd9b48e8b25ef340ce244fb
s-manikanta8 <msonga@deloitte.com>
2024-09-10 16:31:06 +0530

    Add files via upload

---
+++ Assignment7WebApplication/WebApplication5/Controllers/ProductApiController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        public SalesDbContext _context;
+
+        public ProductApiController(SalesDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            List<Product> Product = _context.Products.ToList();
+            return Ok(Product);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound(new { status = "Requested Product details does not exists." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product obj)
+        {
+            _context.Products.Add(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "New Product details added in server." });
+        }
+
+        [HttpPut]
+        public IActionResult EditProduct(Product obj)
+        {
+            _context.Products.Update(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "Product details updated in server." });
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProductById(int id)
+        {
+            Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+                return Ok(new { status = "Product details are deleted from server." });
+            }
+            else
+            {
+                return NotFound(new { status = "Requested Product details does not exists." });
+            }
+        }
+
+    }
+}
---
+++ Assignment7WebApplication/WebApplication5/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+    public class SalesDbContext : DbContext
+    {
+        // This property refer the databse table 
+        // Multiple tables required multiple properties
+        public DbSet<Product> Products { get; set; }
+
+        public SalesDbContext(DbContextOptions<SalesDbContext> options)
+         : base(options)
+        {
+
+        }
+
+
+    }
+}
